e6738ef8becc70453381bd2ca13891975434b3a2
Val <valeryxbaturin@gmail.com>
2020-01-03 18:55:38 +0300

    Add Block structure
    Introduce a prototype of the block structure
    Create an example block
    Init message aka transaction with the specific signature from its author.

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import (
 	"golang.org/x/crypto/nacl/sign"
 )
 
+type Block struct {
+	PrevBlock []byte
+	Author    [32]byte
+	Message   []byte
+	Nonce     int
+}
+
 type Tx struct {
 	From   [32]byte
 	To     [32]byte
@@ -14,20 +21,25 @@ type Tx struct {
 }
 
 func main() {
-	fromPub, _, _ := sign.GenerateKey(rand.Reader)
+	fromPub, fromPr, _ := sign.GenerateKey(rand.Reader)
 	toPub, _, _ := sign.GenerateKey(rand.Reader)
 	var tx = Tx{From: *fromPub, To: *toPub, Amount: 10}
-	fmt.Println(tx)
+	//fmt.Println(tx)
 
 	txJson, _ := json.Marshal(&tx)
 
-	var m Tx
-	json.Unmarshal([]byte(txJson), &m)
-	fmt.Println(m)
+	//var m Tx
+	//json.Unmarshal([]byte(txJson), &m)
+	//fmt.Println(m)
 
-	//signedMessage := sign.Sign(nil, data, fromPr)
-	//message, _ := sign.Open(nil, signedMessage, fromPub)
-	//fmt.Println(tx)
-	//fmt.Println(message)
+	signedMessage := sign.Sign(nil, txJson, fromPr)
+	message, _ := sign.Open(nil, signedMessage, fromPub)
+	var m Tx
+	json.Unmarshal([]byte(message), &m)
+	//fmt.Println(m)
+
+	var prev []byte
+	var block = Block{PrevBlock: prev, Author: *fromPub, Message: signedMessage, Nonce: 0}
+	fmt.Println(block)
 
 }
